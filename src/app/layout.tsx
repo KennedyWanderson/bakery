e6bd2fdd5aa5db2e21585160fdd85f9be6412e3f
KennedyWanderson <kennedy.wanderson410@gmail.com>
2025-05-08 15:26:07 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Montserrat } from 'next/font/google'
+
+const font = Montserrat ({
+  subsets: ["latin"]
+})
+
+export const metadata: Metadata = {
+  title: "Bakery",
+  description: "Building...",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
